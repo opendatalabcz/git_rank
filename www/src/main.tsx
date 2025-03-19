0714591fcc87db45802898324f60e8874b196b51
Alexander Žibrita <alex.zibrita@gmail.com>
2025-03-19 17:40:46 +0100

    Add footer

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -3,9 +3,11 @@ import './main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
 import React from 'react'
+import Footer from './components/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>
 )
