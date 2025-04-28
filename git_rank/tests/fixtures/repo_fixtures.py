6206867d96d3f6c1d73d68157f9c3e3c540bebac
Alexander Žibrita <alex.zibrita@gmail.com>
2025-04-28 16:55:03 +0200

    Complete GitLocalService unit tests

--- git_rank/tests/fixtures/repo_fixtures.py
+++ git_rank/tests/fixtures/repo_fixtures.py
@@ -3,12 +3,33 @@ from typing import cast
 from unittest.mock import MagicMock, create_autospec
 
 import pytest
-from git import Repo
+from git import Commit, Repo
+
+from tests.fixtures.user_data_fixtures import TEST_USER_EMAIL, TEST_USER_NAME
+
+
+@pytest.fixture(name="commit_fixture")
+def commit_fixture() -> MagicMock:
+    commit = cast(MagicMock, create_autospec(Commit, instance=True))
+    commit.author.name = TEST_USER_NAME
+    commit.author.email = TEST_USER_EMAIL
+
+    return commit
+
+
+@pytest.fixture(name="commit_another_author_fixture")
+def commit_another_author_fixture() -> MagicMock:
+    commit = cast(MagicMock, create_autospec(Commit, instance=True))
+    commit.author.name = "NOT" + TEST_USER_NAME
+    commit.author.email = "NOT" + TEST_USER_EMAIL
+
+    return commit
 
 
 @pytest.fixture(name="repo_fixture")
-def repo_fixture() -> MagicMock:
+def repo_fixture(commit_fixture: MagicMock, commit_another_author_fixture: MagicMock) -> MagicMock:
     repo = cast(MagicMock, create_autospec(Repo, instance=True))
     repo.clone_from.return_value = repo
+    repo.iter_commits.return_value = [commit_fixture, commit_another_author_fixture]
 
     return repo
--- git_rank/tests/unit/services/test_git_local_service.py
+++ git_rank/tests/unit/services/test_git_local_service.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, create_autospec
 
 import pytest
 
+from git_rank.models.local_repository import LocalRepository
 from git_rank.models.remote_repository import RemoteRepository
 from git_rank.repositories.git_local.git_local_repository import GitLocalRepository
 from git_rank.services.git_local_service import GitLocalService
@@ -30,3 +31,42 @@ def test_clone_repository(
     git_local_service_fixture.clone_repository(remote_repository_fixture)
 
     git_local_repository_fixture.clone_repository.assert_called_once_with(remote_repository_fixture)
+
+
+def test_remove_repository(
+    git_local_service_fixture: GitLocalService,
+    git_local_repository_fixture: MagicMock,
+    local_repository_fixture: LocalRepository,
+) -> None:
+    git_local_service_fixture.remove_repository(local_repository_fixture)
+
+    git_local_repository_fixture.delete_repository.assert_called_once_with(local_repository_fixture)
+
+
+def test_remove_user_repositories(
+    git_local_service_fixture: GitLocalService,
+    git_local_repository_fixture: MagicMock,
+) -> None:
+    USERNAME = "TEST_USERNAME"
+
+    git_local_service_fixture.remove_user_repositories(USERNAME)
+
+    git_local_repository_fixture.delete_repositories_by_user.assert_called_once_with(USERNAME)
+
+
+def test_filter_user_commits(
+    git_local_service_fixture: GitLocalService,
+    local_repository_fixture: LocalRepository,
+    repo_fixture: MagicMock,
+) -> None:
+
+    user_commits = git_local_service_fixture.filter_user_commits(local_repository_fixture)
+
+    repo_fixture.iter_commits.assert_called_once()
+
+    for user_commit in user_commits:
+        assert (
+            user_commit.author.name == local_repository_fixture.user.user_name
+            or user_commit.author.name == local_repository_fixture.user.username
+            or user_commit.author.email == local_repository_fixture.user.user_email
+        )
