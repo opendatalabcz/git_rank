e2cbffc227c0c6a7e205338074fa324f76a318b1
Alexander Žibrita <alex.zibrita@gmail.com>
2025-04-29 16:14:21 +0200

    Unit test URL method in RepositoryCloner

--- git_rank/tests/unit/application/test_repository_cloner.py
+++ git_rank/tests/unit/application/test_repository_cloner.py
@@ -11,12 +11,14 @@ from git_rank.services.git_local_service import GitLocalService
 from git_rank.services.git_remote_service import GitRemoteService
 
 TEST_USERNAME = "test-user"
+TEST_REPOSITORY_URL = f"https://www.example.com/{TEST_USERNAME}/test-repo.git"
 
 
 @pytest.fixture(name="git_remote_service_fixture")
 def git_remote_service_fixture(remote_repository_fixture: RemoteRepository) -> MagicMock:
     git_remote_service = cast(MagicMock, create_autospec(GitRemoteService, instance=True))
     git_remote_service.get_user_repositories.return_value = [remote_repository_fixture]
+    git_remote_service.get_user_repository_by_url.return_value = remote_repository_fixture
 
     return git_remote_service
 
@@ -52,3 +54,23 @@ def test_clone_repositories(
     git_local_service_fixture.clone_repository.assert_called_once_with(remote_repository_fixture)
 
     assert cloned_repositories == [local_repository_fixture]
+
+
+def test_clone_user_repository(
+    git_remote_service_fixture: MagicMock,
+    git_local_service_fixture: MagicMock,
+    remote_repository_fixture: RemoteRepository,
+    local_repository_fixture: LocalRepository,
+    repository_cloner_fixture: RepositoryCloner,
+) -> None:
+
+    cloned_repository = repository_cloner_fixture.clone_user_repository(
+        username=TEST_USERNAME, repository_url=TEST_REPOSITORY_URL
+    )
+
+    git_remote_service_fixture.get_user_repository_by_url.assert_called_once_with(
+        username=TEST_USERNAME, repository_url=TEST_REPOSITORY_URL
+    )
+    git_local_service_fixture.clone_repository.assert_called_once_with(remote_repository_fixture)
+
+    assert cloned_repository == local_repository_fixture
